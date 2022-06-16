61c1fbb4b16ee9969d8c58b7305d08e7458caa0f
sammy19956 <samsonogberawhe@gmail.com>
2022-06-16 21:52:25 +0100

    My first commit

---
+++ src/Encaps.java
@@ -0,0 +1,13 @@
+public class Encaps {
+    public static void main(String[] args){
+        Human John = new Human();
+       John.setName("juh");
+       John.setAge(5);
+
+
+
+        System.out.println(John.setName());
+
+    }
+
+}
